589083763c09e923195e8fe1987917524a428d9e
Alaguraj <alaguraj.m77@gmail.com>
2026-02-27 21:58:59 +0530

    practice

---
+++ operators/arith.java
@@ -0,0 +1,20 @@
+class arith
+{
+	public static void main(String[]args)
+	{
+		
+		int a = 10;
+		int b = 20;
+		int c=a+b;
+		System.out.println("addition :"+c);
+		int d=a-b;
+		System.out.println("subracton :"+d);
+		int e=a*b;
+		System.out.println("multipilcation :"+e);
+		int f=a/b;
+		System.out.println("division :"+f);
+		
+		
+		
+	}
+}
---
+++ operators/relational.java
@@ -0,0 +1,25 @@
+class relational
+{
+	public static void main(String[]args)
+	{
+        int a = 10;
+		int b = 20;
+		boolean equal = a==b;
+		System.out.println("equal:"+equal);
+		boolean notequal = a!=b;
+
+		System.out.println("notequal :"+notequal);
+		boolean greatherthan = a>b;
+		System.out.println("greatherthan :"+greatherthan);
+		boolean less = a<b;
+		System.out.println("less :"+less);
+		
+
+
+
+
+		
+	
+	
+	}
+}
